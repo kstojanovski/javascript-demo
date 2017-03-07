ab64f83a6d167a47ffef59d901d6755da0225d0b
kstojanovski <stojanovski.kosta@gmail.com>
2017-03-07 18:53:07 +0100

    adding example about the arguments.callee property

---
+++ javascript-demo/basic/callee.js
@@ -0,0 +1,20 @@
+/**
+ * Examples about the arguments.callee function.
+ */
+//arguments.callee property contains the currently executing function
+function factorial(n){
+  if (n <= 0)
+     return 1;
+  else
+     return n * arguments.callee(n - 1)
+}
+console.log(factorial(5));
+function create() {
+	   return function(n) {
+	      if (n <= 1)
+	         return 1;
+	      return n * arguments.callee(n - 1);
+	   };
+	}
+var result = create()(5);
+console.log(result);
